1f59aaa85586c0a9f3a51cba0da2b49509b8754c
Erik Billing <erik.billing@his.se>
2018-02-23 17:01:49 +0100

    Updated finger game

--- TextileControl/patchGUI_pepper.py
+++ TextileControl/patchGUI_pepper.py
@@ -12,12 +12,61 @@ from kivy.clock import Clock
 
 from matplotlib import pyplot as plt
 import numpy as np
+import threading
 
 from patchReader import PatchReader
-from naoqi import ALProxy
+from naoqi import ALProxy, ALModule, ALBroker
 import time
 import patchConstants as pc
 
+broker = ALBroker("pythonBroker","0.0.0.0",0,pc.ROBOTIP,pc.ROBOTPORT)
+
+class Pepper(object):
+    """docstring for Pepper"""
+    def __init__(self):
+        self.memory = ALProxy('ALMemory')
+        self.speech = ALProxy('ALTextToSpeech')
+        self.speechRecog = ALProxy('ALSpeechRecognition')
+        self.posture = ALProxy('ALRobotPosture')
+        self.motion = ALProxy('ALMotion')
+        #movement = ALProxy('ALAutonomousMovement',pc.ROBOTIP,pc.ROBOTPORT)
+        self.life = ALProxy('ALAutonomousLife')
+
+    def say(self,text):
+        thread = threading.Thread(target=lambda: self.speech.say(text))
+        thread.start()
+
+    def setVocabulary(self,words,enabled):
+        self.speechRecog.pause(True)
+        self.speechRecog.setVocabulary(words,enabled)
+        self.speechRecog.pause(False)
+
+    def subscribe(self,event,obj,method):
+        self.memory.subscribeToEvent(event,obj,method)
+
+pepper = Pepper() # Single static instance
+
+class FingerGame(ALModule):
+    """Bulleri Bulleri Bock"""
+    #def __init__(self,*args):
+        #self.waitingForTouch = True
+
+
+    def callback(self, dt):
+        if not self.waitingForTouch: return
+        fingerCount = MainWindow.result
+        if fingerCount:
+            self.waitingForTouch = False
+            pepper.say('Oh! I think I felt %d fingers!'%fingerCount)
+            pepper.posture.goToPosture('Stand',0.5)
+            pepper.say('Am I right?')
+
+    def onWordRecognized(self, key, value):
+        print key,  value
+fingerGame = FingerGame('fingerGame')
+pepper.setVocabulary(['Yes','No'],True)
+pepper.speechRecog.subscribe('my_subsriber')
+print pepper.subscribe("WordRecognized", 'fingerGame', "onWordRecognized")
 
 class OptionScreen(Screen):
     def __init__(self, **kwargs):
@@ -37,15 +86,6 @@ class TaskBox(GridLayout):
         MainWindow.result, MainWindow.img = MainWindow.reader.read()
         print('result in GUI', MainWindow.result)
 
-        # if MainWindow.reader.taskType == 'fingerGame':
-        #     MainWindow.speech.say(pc.NUMBER_OF_FINGERS[MainWindow.result])
-        # elif MainWindow.reader.taskType == 'move':
-        #     MainWindow.speech.say(pc.MOVE_VOICE[MainWindow.result])
-        #     move = pc.MOVE_MAP.get(MainWindow.result,(0,0))
-        #     speed = 0.3
-        #     MainWindow.motion.moveTo(move[0]*speed,0,move[1]*speed,1)
-        # elif MainWindow.reader.taskType == 'hug':
-        #     MainWindow.speech.say(pc.REACTION[MainWindow.result])
         if hasattr(self,'img'):
             self.img.set_data(MainWindow.img)
         else:
@@ -55,26 +95,32 @@ class TaskBox(GridLayout):
 
     def pepperCallback(self, dt):
         if MainWindow.reader.taskType == 'fingerGame':
-            MainWindow.speech.say(pc.NUMBER_OF_FINGERS[MainWindow.result])
+            pepper.say(pc.NUMBER_OF_FINGERS[MainWindow.result])
         elif MainWindow.reader.taskType == 'move':
-            MainWindow.speech.say(pc.MOVE_VOICE[MainWindow.result])
+            pepper.say(pc.MOVE_VOICE[MainWindow.result])
             move = pc.MOVE_MAP.get(MainWindow.result,(0,0))
             speed = 0.3
-            MainWindow.motion.moveTo(move[0]*speed,0,move[1]*speed,1)
+            pepper.motion.moveTo(move[0]*speed,0,move[1]*speed,1)
         elif MainWindow.reader.taskType == 'hug':
-            MainWindow.speech.say(pc.REACTION[MainWindow.result])
+            pepper.say(pc.REACTION[MainWindow.result])
         #time.sleep(1)
 
     def pepperInAction(self, taskType):
-        MainWindow.speech.say(pc.TASKPRESENTATION[taskType])
-        success = MainWindow.posture.goToPosture(pc.TASKPOSE[taskType],0.5)
+        pepper.say(pc.TASKPRESENTATION[taskType])
+        success = pepper.posture.goToPosture(pc.TASKPOSE[taskType],0.5)
 
         MainWindow.sm.current = 'plot'
         plt.ion()
         MainWindow.reader.taskType = taskType
         Clock.schedule_once(self.readerCallback)
         MainWindow.readerEvent = Clock.schedule_interval(self.readerCallback2, 1/25)
-        MainWindow.pepperEvent = Clock.schedule_interval(self.pepperCallback, 1/10)
+
+        if MainWindow.reader.taskType == 'fingerGame':
+            MainWindow.pepperEvent = Clock.schedule_interval(fingerGame.callback, 1/10)
+        elif MainWindow.reader.taskType == 'move':
+            MainWindow.pepperEvent = Clock.schedule_interval(self.pepperCallback, 1/10)
+        elif MainWindow.reader.taskType == 'hug':
+            MainWindow.pepperEvent = Clock.schedule_interval(self.pepperCallback, 1/10)
 
     def hugPepper(self):
         self.pepperInAction('hug')
@@ -117,13 +163,12 @@ class MainWindow(BoxLayout):
     result = 0
     img = np.zeros((20,20))
 
-    speech = ALProxy('ALTextToSpeech',pc.ROBOTIP,pc.ROBOTPORT)
-    posture = ALProxy('ALRobotPosture',pc.ROBOTIP,pc.ROBOTPORT)
-    motion = ALProxy('ALMotion',pc.ROBOTIP,pc.ROBOTPORT)
-
     def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
-        MainWindow.posture.goToPosture('Stand',0.5)
+        #MainWindow.life.setState('disabled')
+        pepper.life.setAutonomousAbilityEnabled('BackgroundMovement',False)
+        pepper.say('Hello everyone! I am the Pepper robot! I am very happy to be here and show off my skills!')
+        pepper.posture.goToPosture('Stand',0.5)
         self.sm.add_widget(OptionScreen(name='option'))
         self.sm.add_widget(PlotScreen(name='plot'))
 
